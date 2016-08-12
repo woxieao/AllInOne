82f965fa04e1523f5f6a4af1b509b36327f0f17a
woxieao <xieao@huiber.cn>
2016-08-12 12:57:36 +0800

    init RegexBrain

---
+++ AlexXieBrain/AlexXieBrain/RegexBrain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AlexXieBrain
+{
+    public class RegexBrain
+    {
+        protected virtual void MatchToRun(string str, Regex reg, Action<string> act)
+        {
+        }
+        protected virtual void NotMatchToRun(string str, Regex reg, Action<string> act)
+        {
+        }
+    }
+}
