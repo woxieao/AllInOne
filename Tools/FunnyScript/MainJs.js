02ea048cafd7c9becb87bcbdc4eb43ee9d85476e
woxieao <xieao@huiber.cn>
2016-11-25 13:18:56 +0800

    优化代码

--- Tools/FunnyScript/MainJs.js
+++ Tools/FunnyScript/MainJs.js
@@ -11,17 +11,19 @@
             element = document.createElement('script');
             element.innerHTML = code;
         } else {
-            var divCss = info.Css === undefined ? "position:fixed; bottom:0; left:0;width:30%;height:250px;background-color:#DEB887;z-index:9999999" : info.Css;
+            var color=parseInt(Math.random()*1000);
+            var divCss = info.Css === undefined ? "position:fixed; padding: 5px;bottom:0; left:0;width:35%;height:250px;background-color:#DEB"+color+";z-index:9999999" : info.Css;
             element = document.createElement('div');
             var title = info.Title === undefined ? "" : info.Title;
             element.innerHTML = "<div style=\"height:20px;width:100%\"><span style=\"float:left\">" + title
 + "</span><span style=\"cursor: pointer;float:right\" onclick=\"document.getElementById('" + scriptId + "').remove();document.getElementById('" + mainId + "').remove()\">X&nbsp;&nbsp; </span></div><div style=\"width:100%\">" + code + "</div>";
             element.style = divCss;
             var scriptList = element.getElementsByTagName("script");
+            var tempScript;
+            tempScript = document.createElement('script');
+            tempScript.id = scriptId;
             if (scriptList.length > 0) {
                 var scriptCodeList = "";
-                var tempScript = document.createElement('script');
-                tempScript.id = scriptId;
                 for (var i = 0; i < scriptList.length; i++) {
                     scriptCodeList += scriptList[i].innerHTML + "\n";
                 }
@@ -29,30 +31,38 @@
                     scriptList[i].remove();
                 }
                 tempScript.innerHTML = scriptCodeList;
-                document.head.appendChild(tempScript);
             }
         };
         element.id = mainId;
         document.body.appendChild(element);
+        document.head.appendChild(tempScript);
         return myId;
     }
 
-    function EncodeCodeFunc(isJs) {
+    function EncodeCodeFunc(isJs,me) {
+
         var createElementStr = CreateElement.toString();
-        var userCode = document.getElementById("sourceCode").value;
+        var userCode =document.getElementsByName("sourceCode")[document.getElementsByName("sourceCode").length-1].value;
         var createElementEncodeStr = window.btoa(escape(createElementStr));
         var createElementScript = "eval(unescape(window.atob('" + createElementEncodeStr + "')));";
         var codResult = window.btoa(escape(userCode));
-        var info = JSON.stringify({ Code: codResult, IsScript: isJs, IsEncode: true });
-        var baseCode = "javascript:" + createElementScript + "CreateElement(" + info + ")";
-        document.getElementById("encodeResult").value = baseCode;
+        var info={ Code: codResult, IsScript: isJs, IsEncode: true };
+        var infoStr = JSON.stringify(info);
+        var baseCode = "javascript:" + createElementScript + "CreateElement(" + infoStr + ")";
+        document.getElementsByName("encodeResult")[document.getElementsByName("encodeResult").length-1].value=baseCode;
+        if(document.getElementsByName("showResult")[document.getElementsByName("showResult").length-1].checked)
+        {
+        info.Css="position:fixed; padding: 5px;bottom:0; right:0;width:35%;height:250px;background-color:yellowgreen;z-index:9999999"
+        CreateElement(info);
+        }
     }
 
     function EncodeCodeAsShortCut() {
-        var code = "<textarea style='width:99%;height:100px' placeholder='JS/Html(JS代码会替换回车为空格,请勿漏写\";\")' id='sourceCode'></textarea><br/>" +
+        var code = "<textarea style='width:100%;height:100px' placeholder='JS/Html' name='sourceCode'></textarea><br/>" +
             "<button style='float:left' onclick='EncodeCodeFunc(true)'>EncodeJsShortCut</button>" +
+            "ShowResult<input name='showResult' type='checkbox' checked='checked'/>"+
             "<button  style='float:right' onclick='EncodeCodeFunc(false)'>EncodeHtmlShortCut</button>" +
-            "<textarea  placeholder='把转换后的内容新建为书签即可快捷使用脚本' onfocus='this.select()' style='width:99%;height:100px' id='encodeResult'></textarea>";
+            "<textarea  placeholder='把转换后的内容新建为书签即可快捷使用脚本' onfocus='this.select()' style='width:100%;height:100px' name='encodeResult'></textarea>";
         CreateElement({ Code: code });
     }
     EncodeCodeAsShortCut();
