59ac5c74f4a915c5497f3f4fc6bfd71c439e0be6
woxieao <xieao@huiber.cn>
2017-04-14 17:12:31 +0800

    add class  ToDynamic/List method ~\(≧▽≦)/~

--- AlexXieBrain/AlexXieBrain/ExtensionCore.cs
+++ AlexXieBrain/AlexXieBrain/ExtensionCore.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
 namespace AlexXieBrain
 {
@@ -7,6 +14,101 @@ namespace AlexXieBrain
         public static string GetTimeStamp()
         {
             return DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+        }
+        private static void SetVal<T>(T sourceDataRef, IDictionary<string, object> resultRef) where T : class
+        {
+            var localData = sourceDataRef.SimpleClone();
+            foreach (var pi in localData.GetType().GetProperties())
+            {
+                var val = pi.GetValue(localData);
+                resultRef[pi.Name] = val;
+            }
+        }
+        /// <summary>
+        /// create a new dynamic object not reference sourceData
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sourceData">sourceData</param>
+        /// <returns>new dynamic object not reference sourceData</returns>
+        public static dynamic ToDynamic<T>(this T sourceData) where T : class
+        {
+            IDictionary<string, object> result = new ExpandoObject();
+            SetVal(sourceData, result);
+            return result;
+        }
+        /// <summary>
+        /// create a new dynamic object not reference sourceData
+        /// </summary>
+        /// <typeparam name="T0"></typeparam>
+        /// <typeparam name="T1"></typeparam>
+        /// <param name="sourceData">sourceData</param>
+        /// <param name="combineVal">combineValue</param>
+        /// <returns>new dynamic object not reference sourceData</returns>
+        public static dynamic ToDynamic<T0, T1>(this T0 sourceData, T1 combineVal)
+            where T0 : class
+            where T1 : class
+        {
+            IDictionary<string, object> result = new ExpandoObject();
+            SetVal(sourceData, result);
+            SetVal(combineVal, result);
+            return result;
+        }
+
+        public static dynamic ToDynamic<T0, T1, T2>(this T0 sourceData, T1 combineVal0, T2 combineVal1)
+            where T0 : class
+            where T1 : class
+            where T2 : class
+        {
+            IDictionary<string, object> result = new ExpandoObject();
+            SetVal(sourceData, result);
+            SetVal(combineVal0, result);
+            SetVal(combineVal1, result);
+            return result;
+        }
+        public static IEnumerable<dynamic> ToDynamicList<T>(this IEnumerable<T> sourceDataList) where T : class
+        {
+            var resultList = new List<IDictionary<string, object>>();
+            foreach (var sourceData in sourceDataList)
+            {
+                resultList.Add(sourceData.ToDynamic());
+            }
+            return resultList;
+        }
+        public static IEnumerable<dynamic> ToDynamicList<T0, T1>(this IEnumerable<T0> sourceDataList, Func<T0, T1> func)
+            where T0 : class
+            where T1 : class
+        {
+            var resultList = new List<IDictionary<string, object>>();
+            foreach (var sourceData in sourceDataList)
+            {
+                resultList.Add(sourceData.ToDynamic(func(sourceData)));
+            }
+            return resultList;
+        }
+
+        public static T SimpleClone<T>(this T source)
+        {
+            var serialized = JsonConvert.SerializeObject(source);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+
+        public static T DeepClone<T>(this T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", nameof(source));
+            }
+            if (ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
         }
     }
 }
--- SoyWeb/SoyWeb/Controllers/HomeController.cs
+++ SoyWeb/SoyWeb/Controllers/HomeController.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.ComTypes;
 using System.Web;
 using System.Web.Mvc;
+using AlexXieBrain;
+using Microsoft.Office.Interop.Excel;
 using Newtonsoft.Json;
 using NPOI.XWPF.Extractor;
 using NPOI.XWPF.UserModel;
+using Action = System.Action;
+using Point = System.Drawing.Point;
 
 namespace SoyWeb.Controllers
 {
+    public static class ModelHelper
+    {
+
+
+    }
+
     public class HomeController : Controller
     {
 
@@ -21,14 +32,60 @@ namespace SoyWeb.Controllers
             var doc = new XWPFDocument(file.BaseStream);
             XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
             String text = extractor.Text;
+            dynamic a;
+
         }
 
+        [Serializable]
+        public class MyClass
+        {
+            private int _a = DateTime.Now.Millisecond;
+            public int A { get; set; }
+            public MyClass2 ClassA { get; set; }
+        }
+
+        [Serializable]
+        public class MyClass2
+        {
+            public int A { get; set; }
+        }
+        public class DyClass : DynamicObject
+        {
+        }
+        public dynamic Test2(dynamic t)
+        {
+
+            return t;
+        }
         public ActionResult Index()
         {
+
+            var a = new MyClass
+            {
+                A = 1,
+                ClassA = new MyClass2
+                {
+                    A = 2
+                }
+
+            };
+            //var ccccc = a.SimpleClone();
+            //var ccccc2 = a.DeepClone();
+            int aaasasas = 1 | 2;
+            var x = (aaasasas == 222 == true);
+
+            var b = a.ToDynamic(new { a = 1, c = 3 }, new { a = 2, ccc = 3 });
+            a.A = 11;
+            a.ClassA.A = 22;
+
+            Test2(new ExpandoObject());
+
+            return View();
+            ViewBag.aa = 2;
             var file = new StreamReader(@"C:\Data\Qfile\计分牌修改.docx");
             var doc = new XWPFDocument(file.BaseStream);
             XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
-            
+
             String text = extractor.Text;
             Response.Write(text);
             return View();
